f8f261f4e6579b94bd3a31beef752200f033de21
gusenkovs <gusenkovs@list.ru>
2016-05-08 20:42:45 +0300

    Start

---
+++ StatisticServer/Default.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="StatisticServer.Default" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1
+        {
+            width: 100%;
+        }
+        .auto-style2
+        {
+            width: 163px;
+        }
+        .auto-style3
+        {
+            width: 730px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table class="auto-style1">
+            <tr>
+                <td colspan="3">&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style3">Cтатистика<br />
+                    Учителя<br />
+                    <asp:Table ID="TeacherList" runat="server" BorderStyle="Solid" CaptionAlign="Bottom" ClientIDMode="AutoID" GridLines="Both">
+                        <asp:TableRow runat="server">
+                            <asp:TableCell runat="server">Имя</asp:TableCell>
+                            <asp:TableCell runat="server">Баллы</asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                    <br />
+                    Группы<br />
+                    <asp:Table ID="GroupList" runat="server" BorderStyle="Solid" CaptionAlign="Bottom" ClientIDMode="AutoID" GridLines="Both">
+                        <asp:TableRow runat="server">
+                            <asp:TableCell runat="server">Имя</asp:TableCell>
+                            <asp:TableCell runat="server">Баллы</asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                    <br />
+                    Студенты<br />
+                    <asp:Table ID="StudentList" runat="server" BorderStyle="Solid" CaptionAlign="Bottom" ClientIDMode="AutoID" GridLines="Both">
+                        <asp:TableRow runat="server">
+                            <asp:TableCell runat="server">Имя</asp:TableCell>
+                            <asp:TableCell runat="server">Баллы</asp:TableCell>
+                            <asp:TableCell runat="server">Прогулы</asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ StatisticServer/Default.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StatisticServer
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        SQLConnectTableAdapters.TbalsTableAdapter sqlTeacherBalls;
+        SQLConnectTableAdapters.TeacherTableAdapter sqlTeacher;
+        SQLConnectTableAdapters.SbalsTableAdapter sqlStudentBalls;
+        SQLConnectTableAdapters.StudentTableAdapter sqlStudent;
+        SQLConnectTableAdapters.GbalsTableAdapter sqlGroupBalls;
+        SQLConnectTableAdapters.GroupTableAdapter sqlGroup;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            sqlTeacher = new SQLConnectTableAdapters.TeacherTableAdapter();
+            sqlTeacherBalls = new SQLConnectTableAdapters.TbalsTableAdapter();
+            sqlStudent = new SQLConnectTableAdapters.StudentTableAdapter();
+            sqlStudentBalls = new SQLConnectTableAdapters.SbalsTableAdapter();
+            sqlGroup = new SQLConnectTableAdapters.GroupTableAdapter();
+            sqlGroupBalls = new SQLConnectTableAdapters.GbalsTableAdapter();
+            var q = sqlTeacher.GetData();
+            for (int i = 0; i < q.Rows.Count; i++)
+            {
+                TableRow tr = new TableRow();
+                TableCell tr1 = new TableCell();
+                tr1.Text = (string)q.Rows[i].ItemArray[1];
+                tr.Cells.Add(tr1);
+                tr1 = new TableCell();
+                try
+                {
+                    tr1.Text = ((int)sqlTeacherBalls.SelectBallsT((int)q.Rows[i].ItemArray[0])).ToString();
+                }
+                catch
+                {
+                    tr1.Text = "Отсутствует";
+                }
+                tr.Cells.Add(tr1);
+                TeacherList.Rows.Add(tr);
+                //
+            }
+            var q2 = sqlGroup.GetData();
+            for (int i = 0; i < q2.Rows.Count; i++)
+            {
+                TableRow tr = new TableRow();
+                TableCell tr1 = new TableCell();
+                tr1.Text = (string)q2.Rows[i].ItemArray[1];
+                tr.Cells.Add(tr1);
+                tr1 = new TableCell();
+                try
+                {
+                tr1.Text = ((int)sqlGroupBalls.SelectBallsG((int)q2.Rows[i].ItemArray[0])).ToString();
+                }
+                catch
+                {
+                    tr1.Text = "Отсутствует";
+                }
+                tr.Cells.Add(tr1);
+                GroupList.Rows.Add(tr);
+                //
+            }
+            var q3 = sqlStudent.GetData();
+            for (int i = 0; i < q3.Rows.Count; i++)
+            {
+                TableRow tr = new TableRow();
+                TableCell tr1 = new TableCell();
+                tr1.Text = (string)q3.Rows[i].ItemArray[1];
+                tr.Cells.Add(tr1);
+                tr1 = new TableCell();
+                 try
+                {
+                tr1.Text = ((int)sqlStudentBalls.SelectBallsS((int)q3.Rows[i].ItemArray[0])).ToString();
+                }
+                 catch
+                 {
+                     tr1.Text = "Отсутствует";
+                 }
+                tr.Cells.Add(tr1);
+
+                tr1 = new TableCell();
+                tr1.Text = ((int)sqlStudentBalls.ProgulSum((int)q3.Rows[i].ItemArray[0])).ToString();
+               
+                tr.Cells.Add(tr1);
+
+                StudentList.Rows.Add(tr);
+                //
+            }
+        }
+
+        protected void TeacherList_Load(object sender, EventArgs e)
+        
+        {
+         
+        }
+    }
+}
